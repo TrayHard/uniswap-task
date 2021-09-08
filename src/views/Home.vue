65ca41282fbf9a9527ea9e09909e6d4206e16639
Polyakov Ilya <wwwtrayfrost@gmail.com>
2021-09-08 23:33:56 +0300

    Removed unnecessary code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
-  <hello-world />
+  <div>
+    
+  </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import { Component, Vue } from 'vue-property-decorator'
 
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  })
+@Component
+export default class Home extends Vue {
+}
 </script>
