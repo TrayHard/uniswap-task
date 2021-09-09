c61fea9fe70f454f6a4b7114234692295bd8f59f
Polyakov Ilya <wwwtrayfrost@gmail.com>
2021-09-09 03:09:39 +0300

    Added CoinField component

---
+++ src/components/CoinField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="coinfield"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { ECurrencyList } from "@/enums/enums";
+
+@Component
+export default class CoinField extends Vue {
+  @Prop({ type: String, default: null })
+  currency!: ECurrencyList;
+
+  @Prop({ type: Number, default: 0 })
+  amount!: number;
+
+  @Prop({ type: Number, default: 0 })
+  equivalent!: number;
+}
+</script>
+
+<style lang="scss">
+.coinfield {
+  height: 85px;
+  background: #212429;
+  border: 1px solid #2c2f36;
+  border-radius: 20px;
+
+  &:hover,
+  &:focus {
+    border: 1px solid #40444f;
+  }
+}
+</style>
---
+++ src/enums/enums.ts
@@ -0,0 +1,7 @@
+export enum ECurrencyList {
+  ETH = 'ETH',
+  BTC = 'BTC',
+  DOGE = 'DOGE',
+  ADA = 'ADA',
+  ATOM = 'ATOM',
+}
