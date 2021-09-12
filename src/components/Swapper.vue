874450790d0530b97a1da88382dc4f58d51ec80f
Polyakov Ilya <wwwtrayfrost@gmail.com>
2021-09-12 20:21:13 +0300

    Added swap button

--- src/components/Swapper.vue
+++ src/components/Swapper.vue
@@ -1,39 +1,30 @@
 <template>
-  <v-card
-    width="480px"
-    rounded="xl"
-    class="swapper"
-    color="colorDarkBg"
-  >
-    <v-card-title
-      primary-title
-      class="swapper__header"
-    >
+  <v-card width="480px" rounded="xl" class="swapper" color="colorDarkBg">
+    <v-card-title primary-title class="swapper__header">
       <span class="swapper__header-title">Swap</span>
-      <v-icon color="#bbb">
-        mdi-cog-outline
-      </v-icon>
+      <v-icon color="#bbb"> mdi-cog-outline </v-icon>
     </v-card-title>
 
     <v-card-text>
       <div class="swapper__coins">
-        <CoinField
-          v-model="coindata[0].amount"
-          :currency.sync="coindata[0].name"
-        />
-        <CoinField
-          v-model="coindata[1].amount"
-          :currency.sync="coindata[1].name"
-        />
+        <div class="swapper__coins-wrapper">
+          <CoinField
+            v-model="coindata[0].amount"
+            :currency.sync="coindata[0].name"
+          />
+          <CoinField
+            v-model="coindata[1].amount"
+            :currency.sync="coindata[1].name"
+          />
+        </div>
+        <div class="swapper__coins-button" @click="coindata.reverse()">
+          <v-icon small>mdi-cached</v-icon>
+        </div>
       </div>
     </v-card-text>
 
     <v-card-actions class="swapper__controls">
-      <v-btn
-        block
-        x-large
-        class="swapper__controls-connect"
-      >
+      <v-btn block x-large class="swapper__controls-connect">
         Connect Wallet
       </v-btn>
     </v-card-actions>
@@ -79,9 +70,37 @@ export default class Home extends Vue {
   }
 
   &__coins {
-    display: grid;
-    grid-auto-flow: row;
-    grid-gap: 4px;
+    position: relative;
+
+    &-wrapper {
+      display: grid;
+      grid-auto-flow: row;
+      grid-gap: 4px;
+    }
+
+    &-button {
+      display: flex;
+      width: 32px;
+      height: 32px;
+      padding: 4px;
+      border-radius: 12px;
+      position: absolute;
+      left: calc(50% - 16px);
+      bottom: calc(50% - 16px);
+      background-color: #212429;
+      border: 4px solid #191b1f;
+      justify-content: space-between;
+      align-items: center;
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.8;
+      }
+
+      .v-icon {
+        transform: translateY(1px);
+      }
+    }
   }
 
   .v-card__text {
