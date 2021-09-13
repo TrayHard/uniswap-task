81d3c042e802533f066e5c0e5704da1796872a93
Polyakov Ilya <wwwtrayfrost@gmail.com>
2021-09-13 04:06:00 +0300

    Changed  "common bases" part of modal

--- src/components/CoinChooser.vue
+++ src/components/CoinChooser.vue
@@ -14,18 +14,30 @@
           rounded
           name="coinname"
           placeholder="Search name or paste address"
+          hide-details
         />
         <div class="coinchooser__common">
           <div class="coinchooser__common-title">
-            Common bases <span class="coinchooser__common-hint">?</span>
+            Common bases
+            <v-tooltip bottom content-class="coinchooser__common-tooltip">
+              <template v-slot:activator="{ on, attrs }">
+                <div class="coinchooser__common-hint" v-bind="attrs" v-on="on">
+                  ?
+                </div>
+              </template>
+              <span>These tokens are commonly paired with other tokens</span>
+            </v-tooltip>
           </div>
           <div class="coinchooser__common-content d-flex">
             <v-btn
               class="coinchooser__common-tokenbtn mr-2 mt-2"
               v-for="coin in basicCoins"
               :key="coin.ticker"
+              outlined
             >
-              <img :src="coin.logo" width="24px" class="mr-2" />
+              <v-avatar size="24px" class="mr-2">
+                <img :src="coin.logo" width="24px" />
+              </v-avatar>
               {{ coin.ticker }}
             </v-btn>
           </div>
@@ -35,8 +47,13 @@
       <v-virtual-scroll :items="fullCoinsList" height="700" item-height="56">
         <template v-slot:default="{ item: coin }">
           <v-list-item :key="coin.ticker">
-            <v-list-item-avatar width="24px" height="24px" min-width="24px" class="ml-1 mr-4">
-              <img :src="coin.logo">
+            <v-list-item-avatar
+              width="24px"
+              height="24px"
+              min-width="24px"
+              class="ml-1 mr-4"
+            >
+              <img :src="coin.logo" />
             </v-list-item-avatar>
 
             <v-list-item-content>
@@ -48,7 +65,6 @@
               </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-
         </template>
       </v-virtual-scroll>
 
@@ -69,10 +85,10 @@ import { COINS, ECoinsList } from "@/enums/enums";
 import { getCoinLogo } from "@/utils";
 
 type TFullCoinListItem = {
-  ticker: string,
-  logo: unknown,
-  fullname: string,
-}
+  ticker: string;
+  logo: unknown;
+  fullname: string;
+};
 
 @Component
 export default class CoinChooser extends Vue {
@@ -132,12 +148,55 @@ export default class CoinChooser extends Vue {
 
   &__search {
     font-size: 18px !important;
+    border-radius: 20px !important;
+    margin-bottom: 16px !important;
   }
 
   &__common {
+    &-title {
+      color: #fff;
+      font-size: 14px;
+    }
+
+    &-tooltip {
+      opacity: 1 !important;
+      font-size: 16px !important;
+      background: #2c2f36 !important;
+      border: 1px solid #40444f !important;
+      color: #c3c5cb !important;
+      border-radius: 8px !important;
+      box-shadow: rgb(0 0 0 / 10%) 0px 4px 8px 0px !important;
+      width: 256px !important;
+      padding: 0.6rem 1rem !important;
+      word-break: break-word !important;
+    }
+
+    &-hint {
+      cursor: default;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      width: 18px;
+      height: 18px;
+      font-size: 14px;
+      border-radius: 50%;
+      background: #2c2f36;
+      color: #c3c5cb;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+
     &-content {
       display: flex;
       flex-wrap: wrap;
+    }
+
+    &-tokenbtn {
+      border-radius: 10px;
+      border: 1px solid #40444f;
+      padding: 6px !important;
     }
   }
 
